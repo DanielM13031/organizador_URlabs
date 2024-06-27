5e875f59aa330f3edd7c3145501e052be65f3046
DanielM <danieljosemorales05@gmail.com>
2024-06-26 21:09:05 -0500

    express conectado

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 
@@ -6,14 +9,22 @@ const app = express();
 // settings
 
 app.set('port', process.env.PORT || 4000);
+app.set('frontend', path.join(__dirname, 'frontend'));
 
 
 // routes
 
 
 
-
-
+//middlewares
+app.use(cors());
+app.use(bodyParser.json());
+// FOR REACT
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+// FOR FOREIGNS ROUTES
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
 // static files
 
 
@@ -22,3 +33,5 @@ app.set('port', process.env.PORT || 4000);
 
 // start the server
 app.listen(app.get('port'), () => console.log('Server on port', app.get('port')))
+
+module.exports = app;
